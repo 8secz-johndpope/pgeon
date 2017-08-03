fc0584801d8970dbbcbfd86142070f7c373890c5
bala223344 <bala223344@gmail.com>
2017-08-03 16:19:34 +0530

    added node-mysql-evens support

--- node_files/index.js
+++ node_files/index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var MySQLEvents = require('mysql-events');
+var dsn = {
+  host:     "localhost",
+  user:     "root",
+  password: "password",
+};
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
@@ -18,6 +24,39 @@ io.on('connection', function(socket){
   });
 });
 
+
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:3001');
 });
+
+
+var mysqlEventWatcher = MySQLEvents(dsn);
+var watcher =mysqlEventWatcher.add(
+  'pgeon.questions',
+  function (oldRow, newRow, event) {
+     //row inserted 
+    if (oldRow === null) {
+      //insert code goes here 
+         console.log('inserted');
+        console.log(newRow.fields)
+    }
+ 
+     //row deleted 
+    if (newRow === null) {
+      //delete code goes here 
+         console.log('inserted de');
+    }
+ 
+     //row updated 
+    if (oldRow !== null && newRow !== null) {
+      //update code goes here 
+         console.log('updat');
+        console.log(newRow.fields)
+    }
+ 
+    //detailed event information 
+    //console.log(event) 
+  }
+  
+);
