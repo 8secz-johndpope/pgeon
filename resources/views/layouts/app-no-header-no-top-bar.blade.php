45552fef92b19dcdf89b27968fec0597336e7815
newmac <bala223344@gmail.com>
2018-09-10 15:49:33 +0530

    ln share working

--- resources/views/layouts/app-no-header-no-top-bar.blade.php
+++ resources/views/layouts/app-no-header-no-top-bar.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-    <title> Pgeon </title>
+    <title> {{($title)?$title:"Pgeon"}} </title>
     <meta name="description" content="Messenger for all"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
